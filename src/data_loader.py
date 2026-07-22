51f274cec63a46415061f290a2b6e6b95c90f3cb
huiyu <1691509286@qq.com>
2026-07-22 15:03:03 +0800

    为data_loader.py加入了测试

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,5 +3,9 @@ import numpy as np
 from pathlib import Path
 
 def load_excel(path: Path, sheet_name=0) -> np.ndarray:
-    df=pd.read_excel(path, sheet_name=sheet_name)
+    df=pd.read_excel(path, sheet_name=sheet_name,header=None)
     return df.to_numpy (dtype=np.float64)
+
+#测试
+B= load_excel(Path("C:/Users/16915/Desktop/Problem B/Project/problem1/附件1/B.xlsx"))
+print(B.shape)
